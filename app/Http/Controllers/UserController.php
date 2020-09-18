86c58e64e286b5900a5eb7af3cf96768a56e5af2
AbdElrahmaN31 <abdelrahmannashaat0@gmail.com>
2020-09-18 22:55:49 +0200

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+    use App\Models\User;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Hash;
+
+    class UserController extends Controller
+    {
+        function index(Request $request)
+        {
+            $user = User::where('email', $request->email)->first();
+            // print_r($data);
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response([
+                    'message' => ['These credentials do not match our records.'],
+                ], 404);
+            }
+
+            $token = $user->createToken('my-app-token')->plainTextToken;
+
+            $response = [
+                'user' => $user,
+                'token' => $token,
+            ];
+
+            return response($response, 201);
+        }
+
+        public function show()
+        {
+            return auth()->user();
+        }
+
+        public function logout(Request $request)
+        {
+            // Revoke a specific user token
+            $request->user()->currentAccessToken()->delete();
+            // Get user who requested the logout
+            $user = request()->user(); //or Auth::user()
+            // Revoke current user token
+            $user->tokens()->where('id', $user->currentAccessToken()->id)->delete();
+        }
+
+    }
